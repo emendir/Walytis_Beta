891d4243d50d7455913418a8362ec4abc83bd7a5
Emendir <dev@emendir.tech>
2025-05-18 13:38:27 +0200

    updated readme quick install

--- docs/documentation_validation/check_urls.py
+++ docs/documentation_validation/check_urls.py
@@ -5,12 +5,12 @@ npm install -g markdown-link-check
 """
 import os
 
-from md_utils import get_markdown_files
+from md_utils import get_markdown_files, DOCS_DIR
 
 for md_file in get_markdown_files():
     command = (
         "markdown-link-check "
-        "-c ./docs/documentation_validation/markdown-link-checker.conf -q "
+        f"-c ./{DOCS_DIR}/documentation_validation/markdown-link-checker.conf -q "
         f"{md_file}"
     )
     os.system(command)
